9895c1d4d014309522ad745fd595e24d83ab19f6
Rodrigo Albuquerque <rodrigo.albuquerque.linux@gmail.com>
2024-10-18 22:47:40 -0300

    Create apply01.kt

---
+++ apply01.kt
@@ -0,0 +1,17 @@
+//Uso do .apply{}
+class Pessoa {
+    var nome: String = ""
+    var idade: Int = 0
+
+    fun saudacao(){
+        println("Meu nome é $nome e tenho $idade anos de idade.")
+    }
+}
+
+fun main() {
+    val pessoa = Pessoa().apply {
+        nome = "Rodrigo"
+        idade = 29
+    }
+    pessoa.saudacao()
+}
